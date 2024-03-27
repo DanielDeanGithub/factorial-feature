1a53036972ec8069f2e2ad388d475df2aa9b6a0e
DanielDeanGithub <github-new@inboxrefresh.com>
2024-03-27 09:33:40 +0000

    Refactor .factorial() to be more concise

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 const Calculate = {
     factorial(input) {
-        let output = 1;
-
-        for (let i = input; i > 0; i--) {
-            output *= i;
+        for (let i = input - 1; i > 0; i--) {
+            input *= i;
         }
 
-        return output;
+        return input;
     }
 }
 
