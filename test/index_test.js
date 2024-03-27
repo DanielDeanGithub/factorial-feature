68e9fe23cfbed04efb9cf4137dc2dc28aaf049d9
DanielDeanGithub <github-new@inboxrefresh.com>
2024-03-27 09:15:07 +0000

    Refactor it block to include setup, exercise and verify test phases

--- test/index_test.js
+++ test/index_test.js
@@ -4,7 +4,15 @@ var Calculate =  require('../index.js')
 describe('Calculate', () => {
   describe('.factorial', () => {
     it('test the output of 5! is equal to 120', () => {
-      assert.equal(Calculate.factorial(5), 120);      
+      // Setup
+      const inputNumber = 5;
+      const expectedResult = 120;
+      
+      // Exercise
+      const actualResult = Calculate.factorial(inputNumber);
+
+      // Verify
+      assert.equal(actualResult, expectedResult);      
     }); 
   });
 });
