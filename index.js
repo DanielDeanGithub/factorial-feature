a773609744af737698ee56019e126efa1f6096c3
DanielDeanGithub <github-new@inboxrefresh.com>
2024-03-28 12:19:49 +0000

    Update .factorial() for edge case when input isn't a number

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const Calculate = {
     factorial(input) {
+        if (typeof(input) !== "number") throw new TypeError("Error: input type isn't a number.");
+
         if (input === 0) return 1;
 
         for (let i = input - 1; i > 0; i--) {
